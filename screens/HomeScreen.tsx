1ff3088d8e2fcce11d985018901d835ec8b0a9a9
Matan Shaviro <matan3sh@gmail.com>
2022-01-28 08:15:15 +0200

    first commit

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>I am Home Screen</Text>
+    </View>
+  );
+}
---
+++ screens/PlannerScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function PlannerScreen() {
+  return (
+    <View>
+      <Text>I am Planner Screen</Text>
+    </View>
+  );
+}
