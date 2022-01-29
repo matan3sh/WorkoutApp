53282582d4a79b213da949f1b5fcf20a817e3aa9
Matan Shaviro <matan3sh@gmail.com>
2022-01-29 13:52:15 +0200

    Navigate between screens

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }: any) {
   return (
     <View>
       <Text>I am Home Screen</Text>
+      <Button
+        onPress={() => navigation.navigate("Planner")}
+        title="Go to Planner"
+      />
     </View>
   );
 }
--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-export default function PlannerScreen() {
+export default function PlannerScreen({ navigation }: any) {
   return (
     <View>
       <Text>I am Planner Screen</Text>
+      <Button onPress={() => navigation.navigate("Home")} title="Back Home" />
     </View>
   );
 }
