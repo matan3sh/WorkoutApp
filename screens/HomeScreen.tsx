718694e13124e5c03e214cbca64dc8bcb8240307
Matan Shaviro <matan3sh@gmail.com>
2022-01-30 21:03:44 +0200

    Style the workout list in home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,32 @@
+import { View, StyleSheet, FlatList, Text } from "react-native";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
-import { View, Text, Button } from "react-native";
+
+import WorkoutItem from "../components/WorkoutItem";
+
+import data from "../data.json";
+import { Workout } from "../types/data";
 
 export default function HomeScreen({ navigation }: NativeStackHeaderProps) {
   return (
-    <View>
-      <Text>I am Home Screen</Text>
-      <Button
-        onPress={() => navigation.navigate("Planner")}
-        title="Go to Planner"
+    <View style={styles.container}>
+      <Text style={styles.header}>New Workouts</Text>
+      <FlatList
+        data={data as Workout[]}
+        renderItem={WorkoutItem}
+        keyExtractor={(item) => item.slug}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    flex: 1,
+  },
+  header: {
+    fontSize: 20,
+    marginBottom: 20,
+    fontWeight: "bold",
+  },
+});
