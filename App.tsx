7bf5f2e54d084f7548b6a5b249a8ffce96dd2d13
Matan Shaviro <matan3sh@gmail.com>
2022-01-30 21:26:13 +0200

    Start implementing useCachedResources and load app fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 
+import useCachedResources from "./hooks/useCachedResources";
+
 import Navigation from "./navigation";
 
 export default function App() {
+  const isLoaded = useCachedResources();
+
   return (
     <>
       <Navigation />
