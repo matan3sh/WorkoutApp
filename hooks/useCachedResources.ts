9ef4fe2589cf11a8da84adda40b0007b59ca8119
Matan Shaviro <matan3sh@gmail.com>
2022-02-01 08:38:26 +0200

    Start implement logic for async data storage

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import * as Font from "expo-font";
+import { getData, storeData } from "../storage";
+
+import data from "../data.json";
 
 export default function useCachedResources() {
   const [isLoadingComplete, setIsLoadingComplete] = useState(false);
@@ -7,6 +10,7 @@ export default function useCachedResources() {
   useEffect(() => {
     async function loadResourcesAndDataAsync() {
       try {
+        await storeData("workout-data", data);
         await Font.loadAsync({
           montserrat: require("../assets/fonts/Montserrat-Regular.ttf"),
           "montserrat-bold": require("../assets/fonts/Montserrat-Bold.ttf"),
@@ -14,6 +18,8 @@ export default function useCachedResources() {
       } catch (error) {
         console.warn(error);
       } finally {
+        const workouts = await getData("workout-data");
+        console.log(workouts);
         setIsLoadingComplete(true);
       }
     }
