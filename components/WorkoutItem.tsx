e76fab4ca61325ef8a846b297d033b0b8e7a3ed3
Matan Shaviro <matan3sh@gmail.com>
2022-02-05 17:41:13 +0200

    Implement children to workoutItem component

--- components/WorkoutItem.tsx
+++ components/WorkoutItem.tsx
@@ -1,14 +1,24 @@
-import { Text, View, StyleSheet } from "react-native";
+import { ReactNode } from "react";
+import { Text, View, StyleSheet, StyleProp, ViewStyle } from "react-native";
 
 import { Workout } from "../types/data";
 import { formatSec } from "../utils/time";
 
-export default function WorkoutItem({ item }: { item: Workout }) {
+export default function WorkoutItem({
+  item,
+  children,
+  childStyles = {},
+}: {
+  item: Workout;
+  children?: ReactNode;
+  childStyles?: StyleProp<ViewStyle>;
+}) {
   return (
     <View style={styles.container}>
       <Text style={styles.name}>{item.name}</Text>
       <Text style={styles.duration}>Duration: {formatSec(item.duration)}</Text>
       <Text style={styles.difficulty}>Difficulty: {item.difficulty}</Text>
+      {children && <View style={childStyles}>{children}</View>}
     </View>
   );
 }
--- screens/WorkoutDetailScreen.tsx
+++ screens/WorkoutDetailScreen.tsx
@@ -5,6 +5,7 @@ import { FontAwesome } from "@expo/vector-icons";
 import { useWorkoutBySlug } from "../hooks/useWorkoutBySlug";
 import { Modal } from "../components/styled/Modal";
 import { PressableText } from "../components/styled/PressableText";
+import WorkoutItem from "../components/WorkoutItem";
 import { formatSec } from "../utils/time";
 
 type DetailParams = {
@@ -20,28 +21,32 @@ type Navigation = NativeStackHeaderProps & DetailParams;
 export default function WorkoutDetailScreen({ route }: Navigation) {
   const workout = useWorkoutBySlug(route.params.slug);
 
+  if (!workout) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Name - {workout?.name}</Text>
-
-      <Modal
-        activator={({ handleOpen }) => (
-          <PressableText onPress={handleOpen} text="Check Sequence" />
-        )}>
-        <View>
-          {workout?.sequence.map((sequenceItem, index) => (
-            <View style={styles.sequenceItem} key={sequenceItem.slug}>
-              <Text>
-                {sequenceItem.name} | {sequenceItem.type} |{" "}
-                {formatSec(sequenceItem.duration)}
-              </Text>
-              {index !== workout.sequence.length - 1 && (
-                <FontAwesome name="arrow-down" size={20} />
-              )}
-            </View>
-          ))}
-        </View>
-      </Modal>
+      <WorkoutItem item={workout} childStyles={{ marginTop: 10 }}>
+        <Modal
+          activator={({ handleOpen }) => (
+            <PressableText onPress={handleOpen} text="Check Sequence" />
+          )}>
+          <View>
+            {workout.sequence.map((sequenceItem, index) => (
+              <View style={styles.sequenceItem} key={sequenceItem.slug}>
+                <Text>
+                  {sequenceItem.name} | {sequenceItem.type} |{" "}
+                  {formatSec(sequenceItem.duration)}
+                </Text>
+                {index !== workout.sequence.length - 1 && (
+                  <FontAwesome name="arrow-down" size={20} />
+                )}
+              </View>
+            ))}
+          </View>
+        </Modal>
+      </WorkoutItem>
     </View>
   );
 }
