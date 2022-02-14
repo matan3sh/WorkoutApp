bc1fd9e029f1318c9daf073a8cdb2b8fe9f29043
Matan Shaviro <matan3sh@gmail.com>
2022-02-14 19:55:10 +0200

    Start implementing the Form in the planner screen

---
+++ components/WorkoutForm.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { PressableText } from "./styled/PressableText";
+
+export type ExerciseForm = {
+  name: string;
+  duration: string;
+};
+
+type WorkoutProps = {
+  onSubmit: (form: ExerciseForm) => void;
+};
+
+export default function WorkoutForm({ onSubmit }: WorkoutProps) {
+  const [form, setForm] = useState<ExerciseForm>({
+    name: "",
+    duration: "",
+  });
+
+  const onChangeText = (name: string) => (text: string) => {
+    setForm({
+      ...form,
+      [name]: text,
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Workout Form</Text>
+
+      <View>
+        <TextInput
+          onChangeText={onChangeText("name")}
+          style={styles.input}
+          value={form.name}
+        />
+        <TextInput
+          onChangeText={onChangeText("duration")}
+          style={styles.input}
+          value={form.duration}
+        />
+        <PressableText text="Submit" onPress={() => onSubmit(form)} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,11 +1,22 @@
-import { View, Text, Button } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
+import WorkoutForm, { ExerciseForm } from "../components/WorkoutForm";
 
 export default function PlannerScreen({ navigation }: NativeStackHeaderProps) {
+  const handleFormSubmit = (form: ExerciseForm) => {
+    alert(JSON.stringify(form));
+  };
+
   return (
-    <View>
-      <Text>I am Planner Screen</Text>
-      <Button onPress={() => navigation.navigate("Home")} title="Back Home" />
+    <View style={styles.container}>
+      <WorkoutForm onSubmit={handleFormSubmit} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
